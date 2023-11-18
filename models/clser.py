ea7f4fa87d1fcdb5450d32a170d3cb93613964d1
unknown <ashishkumar0154@gmail.com>
2023-11-18 12:30:29 +0530

    test

--- models/clser.py
+++ models/clser.py
@@ -125,7 +125,7 @@ class CLSER(ContinualModel):
             )
         else:
             self.buffer.add_data(
-                examples=torch.asarray(sorted_input),
+                examples=torch.tensor(sorted_input),
                 labels=outputs_1,        
             )
 
